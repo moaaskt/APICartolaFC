9148c1239c879fdbea225a8de35bd34640f4acc2
MOACIR DA SILVA NETO <moacir_silva-neto@estudante.sesisenai.org.br>
2023-06-12 20:43:38 -0300

    versão final

---
+++ src/App.jsx
@@ -0,0 +1,129 @@
+import AtletasList from "./components/AtletasList";
+import PosicoesList from "./components/PosicoesList";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+const App = () => {
+  const [atletas, setAtletas] = useState([]);
+  const [atletasPesquisados, setAtletasPesquisados] = useState([]);
+  const [clubes, setClubes] = useState([]);
+  const [posicoes, setPosicoes] = useState([]);
+  const [valorPesquisado, setValorPesquisado] = useState("");
+  const [ordenarPorPontuacao, setOrdenarPorPontuacao] = useState(false);
+  const [rodadaSelecionada, setRodadaSelecionada] = useState(1);
+
+  const pesquisarJogador = () => {
+    const atletasPesquisados = atletas.filter((atleta) => {
+      return removerAcentos(atleta.apelido).includes(
+        removerAcentos(valorPesquisado)
+      );
+    });
+
+    if (atletasPesquisados.length === 0) {
+      alert("Atleta não encontrado");
+    } else {
+      if (ordenarPorPontuacao) {
+        atletasPesquisados.sort((a, b) => b.pontuacao - a.pontuacao);
+        console.log(atletasPesquisados);
+      }
+      setAtletasPesquisados(atletasPesquisados);
+    }
+  };
+
+  const selecionarRodada = ({ target }) => {
+    setRodadaSelecionada(target.value);
+  };
+
+  const toggleOrdenarPorPontuacao = () => {
+    setOrdenarPorPontuacao(!ordenarPorPontuacao);
+  };
+
+  const removerAcentos = (str) => {
+    return str
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase();
+  };
+
+  useEffect(() => {
+    if (ordenarPorPontuacao) {
+      const ordernarAtletas = [...atletas]; // Fazer uma cópia do array atletas
+      const atletasOrdenados = ordernarAtletas.sort(
+        (a, b) => b.pontuacao - a.pontuacao
+      );
+      setAtletasPesquisados(atletasOrdenados);
+    }
+  }, [ordenarPorPontuacao]);
+
+  useEffect(() => {
+    const fetchAtletas = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.cartola.globo.com/atletas/pontuados/${rodadaSelecionada}`
+        );
+        setAtletas(Object.values(response.data.atletas));
+        setAtletasPesquisados(Object.values(response.data.atletas));
+        setClubes(Object.values(response.data.clubes));
+        setPosicoes(Object.values(response.data.posicoes));
+        setOrdenarPorPontuacao(false);
+      } catch (error) {
+        console.error("Error fetching atletas:", error);
+      }
+    };
+
+    fetchAtletas();
+  }, [rodadaSelecionada]);
+
+  return (
+    <div>
+      <img
+        className="logopng"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Cartola_FC_logo.svg/2560px-Cartola_FC_logo.svg.png"
+        alt=""
+      />
+      <div className="content">
+        <input
+          title="insira o nome do atleta"
+          onChange={(event) => setValorPesquisado(event.target.value)}
+          type="text"
+          placeholder="Nome do jogador"
+        />{" "}
+        <button title="Pesquisar atleta" onClick={pesquisarJogador}>
+          Pesquisar
+        </button>
+        <button
+          title="Ordendar por maior pontuação"
+          onClick={toggleOrdenarPorPontuacao}
+        >
+          {ordenarPorPontuacao
+            ? "Ordenado por Pontuação"
+            : "Ordenar por Pontuação"}
+        </button>
+        <select
+          title="Selecione a rodada"
+          onChange={(event) => selecionarRodada(event)}
+        >
+          <option value={1}>1º Rodada</option>
+          <option value={2}>2º Rodada</option>
+          <option value={3}>3º Rodada</option>
+          <option>4º Rodada</option>
+          <option>5º Rodada</option>
+          <option>6º Rodada</option>
+          <option>7º Rodada</option>
+          <option>8º Rodada</option>
+          <option>9º Rodada</option>
+        </select>
+      </div>
+      {atletasPesquisados && (
+        <AtletasList
+          atletas={atletasPesquisados}
+          clubes={clubes}
+          posicoes={posicoes}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/AtletasList.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import ClubesList from "./ClubesList";
+import PosicoesList from "./PosicoesList";
+
+const AtletasList = (props) => {
+  const procurarClube = (id) => {
+    const clubeCorreto = props.clubes.find((clube) => {
+      return clube.id == parseInt(id);
+    });
+
+    return clubeCorreto;
+  };
+
+  const procurarPosicao = (id) => {
+    const posicaoCorreta = props.posicoes.find((posicao) => {
+      return posicao.id == parseInt(id);
+    });
+
+    return posicaoCorreta;
+  };
+
+  return (
+    <div className="container">
+      <h2 >Atletas mais pontuados da última rodada</h2>
+      <ul className="list">
+        {props.atletas.map((atleta, index) => (
+          <li key={index} className="list-item player-card">
+            <img src={atleta.foto.replace("FORMATO", "220x220")} alt="" />
+            <img
+              className="club-flag"
+              src={procurarClube(atleta.clube_id).escudos["60x60"]}
+              alt=""
+            />
+            <div className="player-info">
+              <span className="player-name">{atleta.apelido}</span>
+              <br /><span className="player-position">
+                {procurarPosicao(atleta.posicao_id).nome} -{" "}
+                {procurarPosicao(atleta.posicao_id).abreviacao}
+              </span>
+            </div>
+            <span className="player-score">Pontuação: </span> <div className="pontuacao">{atleta.pontuacao}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AtletasList;
