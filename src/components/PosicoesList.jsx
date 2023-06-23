0540c0e405f51330ea23b2ea5da40e7aba2111fc
Moacir Neto <101300315+moaaskt@users.noreply.github.com>
2023-06-23 00:22:56 -0300

    Update PosicoesList.jsx

--- src/components/PosicoesList.jsx
+++ src/components/PosicoesList.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 
 const PosicoesList = (props) => {
@@ -12,18 +11,4 @@ const PosicoesList = (props) => {
 };
 
 export default PosicoesList;
-=======
-import React, { useEffect, useState } from "react";
 
-const PosicoesList = (props) => {
-  return (
-    <div className="container">
-    <span className="posicao">
-      Posição do atleta: {props.posicao.nome} - {props.posicao.abreviacao}
-    </span>
-  </div>
-  );
-};
-
-export default PosicoesList;
->>>>>>> origin/main
